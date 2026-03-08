4c723fc47df4300fdf8e8d4148e8abfe24d9bf8c
Diego DG <doguezdiego@gmail.com>
2026-03-07 21:49:19 -0600

    feat: Implement repository method for last subscription charges

--- src/infrastructure/database/repositories/sqlalchemy_subscription_charge_repository.py
+++ src/infrastructure/database/repositories/sqlalchemy_subscription_charge_repository.py
@@ -3,6 +3,7 @@ from sqlalchemy import and_, asc, extract, desc, select
 from typing import Optional, List
 from datetime import date
 
+from domain.entities import subscription_charge
 from domain.entities.subscription_charge import SubscriptionCharge
 from domain.repositories.subscription_charge_repository import (
     SubscriptionChargeRepository,
@@ -190,3 +191,20 @@ class SQLAlchemySubscriptionChargeRepository(SubscriptionChargeRepository):
             category_model.name if category_model else None,
             account_model.name,
         )
+
+    async def get_last_charges_by_subscription_id(self, subscription_id: int) -> List[SubscriptionCharge, Optional[str]]:
+        stmt = (
+            select(SubscriptionChargeModel, SubscriptionModel, AccountModel)
+            .join(SubscriptionModel, SubscriptionChargeModel.subscription_id == SubscriptionModel.id)
+            .join(AccountModel, SubscriptionModel.account_id == AccountModel.id)
+            .where(SubscriptionChargeModel.subscription_id == subscription_id)
+            .order_by(asc(SubscriptionChargeModel.charge_date))
+            .limit(5)
+        )
+        result = await self.db.execute(stmt)
+        results = result.all()
+
+        return [
+            (self._model_to_entity(subscription_charge), subscription.name, account.name)
+            for subscription_charge, subscription, account in results
+        ]
